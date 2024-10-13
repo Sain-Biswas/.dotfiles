7b808da61767c1755542ded720e75500d8f4463b
Sain-Biswas <sain.biswas614@gmail.com>
2024-10-13 06:27:24 +0530

    Add install script

---
+++ setup.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/bash
+
+sudo pacman -Syyu stow rust
+
+cd ~/.config
+
+rm -rf hypr kitty
+
+cd ~/.dotfiles
+stow .
+
+cd ~
+
+mkdir Github .icons .themes
+cd Github
+
+git clone https://aur.archlinux.org/paru-git.git
+cd paru-git
+makepkg -si
+cd src/paru
+cargo clean
+
+
+pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+pacman-key --lsign-key 3056513887B78AEB
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+sudo sh -c "echo -e '\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist' >> /etc/pacman.conf"
+
+sudo sh -c "echo -e '\n[warpdotdev]\nServer = https://releases.warp.dev/linux/pacman/\$repo/\$arch' >> /etc/pacman.conf"
+sudo pacman-key -r "linux-maintainers@warp.dev"
+sudo pacman-key --lsign-key "linux-maintainers@warp.dev"
+
+sudo pacman -Syy
+
+cd ~/.dotfiles
+
+paru -Rcns dolphin vim nano htop wofi polkit-kde-agent
+
+paru -Syy --needed chaotic-aur/android-studio bat chaotic-aur/brave-bin brightnessctl chromium clang cliphist dunst eslint fastfetch fzf gdb gnome-disk-utility go chaotic-aur/google-chrome grim chaotic-aur/grimblast-git hypridle hyprlock hyprpaper hyprpicker intellij-idea-community-edition kitty lldb loupe chaotic-aur/mongodb-compass nautilus neovim noto-fonts-emoji ntfs-3g nwg-look pavucontrol polkit-gnome chaotic-aur/profile-sync-daemon-brave python-pip python-pywal16 rofi-wayland rust rust-analyzer stow ttf-font-awesome unzip chaotic-aur/visual-studio-code-bin warp-terminal waybar xdg-user-dirs xorg-xwayland zed zoxide zsh
+
+xdg-user-dirs-update
+
+cd ~
