761c0b5f17d078b98df7e96861134e8a1f72f7c1
Sain Biswas <131385862+Sain-Biswas@users.noreply.github.com>
2024-10-16 00:25:42 +0530

    switch to dolphin

--- setup.sh
+++ setup.sh
@@ -42,9 +42,9 @@ sudo pacman -Syy
 
 cd ~/.dotfiles
 
-paru -Rcns dolphin vim nano htop wofi polkit-kde-agent
+paru -Rcns vim nano htop wofi
 
-paru -Syy --needed chaotic-aur/android-studio bat chaotic-aur/brave-bin brightnessctl chromium clang cliphist dunst eslint eza fastfetch fzf gdb gnome-disk-utility go chaotic-aur/google-chrome grim chaotic-aur/grimblast-git hypridle hyprlock hyprpaper hyprpicker intellij-idea-community-edition kitty lldb loupe chaotic-aur/mongodb-compass nautilus neovim noto-fonts-emoji ntfs-3g nwg-look pavucontrol polkit-gnome chaotic-aur/profile-sync-daemon-brave python-pip python-pywal16 rofi-wayland rust rust-analyzer stow ttf-font-awesome unzip chaotic-aur/visual-studio-code-bin warp-terminal waybar xdg-user-dirs xorg-xwayland zed zoxide zsh
+paru -Syy --needed chaotic-aur/android-studio bat chaotic-aur/brave-bin brightnessctl chromium clang cliphist dunst eslint eza fastfetch fzf gdb gnome-disk-utility go chaotic-aur/google-chrome grim chaotic-aur/grimblast-git hypridle hyprlock hyprpaper hyprpicker intellij-idea-community-edition kitty lldb loupe chaotic-aur/mongodb-compass neovim noto-fonts-emoji ntfs-3g nwg-look pavucontrol chaotic-aur/profile-sync-daemon-brave python-pip python-pywal16 rofi-wayland rust rust-analyzer stow ttf-font-awesome unzip chaotic-aur/visual-studio-code-bin warp-terminal waybar xdg-user-dirs xorg-xwayland zed zoxide zsh
 
 xdg-user-dirs-update
 
