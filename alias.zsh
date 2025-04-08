7878bcfa9355fc17bb4f4c03e32fa6da4169dead
Sain-Biswas <sain.biswas614@gmail.com>
2025-04-09 00:32:58 +0530

    feat: add terminal aliases

--- alias.zsh
+++ alias.zsh
@@ -1,3 +1,20 @@
 # ░█▀█░█░░░▀█▀░█▀█░█▀▀░░░░░░░░░▀▀█░█▀▀░█░█
 # ░█▀█░█░░░░█░░█▀█░▀▀█░░░▄▄▄░░░▄▀░░▀▀█░█▀█
 # ░▀░▀░▀▀▀░▀▀▀░▀░▀░▀▀▀░░░░░░░░░▀▀▀░▀▀▀░▀░▀
+
+# Common Apps
+alias vi="nvim"
+alias vim="nvim"
+alias c="clear"
+alias mkdir="mkdir -p"
+alias compass='mongodb-compass --ignore-additional-command-line-flags --password-store="gnome-libsecret" &'
+
+
+# Directory navigation shortcuts
+alias ..='cd ..'
+alias ...='cd ../..'
+
+# eza
+alias ls="eza -Ga"
+alias ll="eza -la"
+alias lt="eza -lTa --git-ignore"
