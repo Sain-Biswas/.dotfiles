5cf7284865d0853d137c362ba4297166fd9c4e53
Sain-Biswas <sain.biswas614@gmail.com>
2025-06-21 22:11:02 +0530

    update: add cli alias for vscode insiders

--- alias.zsh
+++ alias.zsh
@@ -8,7 +8,7 @@ alias vim="nvim"
 alias c="clear"
 alias mkdir="mkdir -p"
 alias compass='mongodb-compass --ignore-additional-command-line-flags --password-store="gnome-libsecret" &'
-
+alias code="code-insiders"
 
 # Directory navigation shortcuts
 alias ..='cd ..'
--- matugen.zsh
+++ matugen.zsh
@@ -6,13 +6,13 @@
 export FZF_DEFAULT_OPTS=" \
 --style full \
 --preview 'bat --color=always {}' --bind 'focus:transform-header:file --brief {}' \
---color=bg+:#3a383e,bg:#141318,spinner:#ffb4ab,hl:#c9c3dc \
---color=fg:#e5e1e9,header:#c9c3dc,info:#ecb8cd,pointer:#ffb4ab \
---color=marker:#c8bfff,fg+:#e5e1e9,prompt:#ecb8cd,hl+:#c9c3dc \
---color=selected-bg:#c8bfff \
---color=border:#938f99,label:#e5e1e9 \
+--color=bg+:#36393e,bg:#101418,spinner:#ffb4ab,hl:#b9c8da \
+--color=fg:#e0e2e8,header:#b9c8da,info:#d4bee6,pointer:#ffb4ab \
+--color=marker:#9ccbfb,fg+:#e0e2e8,prompt:#d4bee6,hl+:#b9c8da \
+--color=selected-bg:#9ccbfb \
+--color=border:#8c9199,label:#e0e2e8 \
 --multi"
 
 
 # Aloxaf fzf - tab
-zstyle ':fzf-tab:*' fzf-flags --color=bg+:#3a383e,bg:#141318,spinner:#ffb4ab,hl:#c9c3dc,fg:#e5e1e9,header:#c9c3dc,info:#ecb8cd,pointer:#ffb4ab,marker:#c8bfff,fg+:#e5e1e9,prompt:#ecb8cd,hl+:#c9c3dc,selected-bg:#c8bfff,border:#938f99,label:#e5e1e9 --multi
+zstyle ':fzf-tab:*' fzf-flags --color=bg+:#36393e,bg:#101418,spinner:#ffb4ab,hl:#b9c8da,fg:#e0e2e8,header:#b9c8da,info:#d4bee6,pointer:#ffb4ab,marker:#9ccbfb,fg+:#e0e2e8,prompt:#d4bee6,hl+:#b9c8da,selected-bg:#9ccbfb,border:#8c9199,label:#e0e2e8 --multi
