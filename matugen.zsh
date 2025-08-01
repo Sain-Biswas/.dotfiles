5d9c3794a88cec453685a4d592ea538e6dbba453
Sain-Biswas <sain.biswas614@gmail.com>
2025-08-01 23:17:52 +0530

    feat: add clipboard with rofi support.

--- matugen.zsh
+++ matugen.zsh
@@ -6,13 +6,13 @@
 export FZF_DEFAULT_OPTS=" \
 --style full \
 --preview 'bat --color=always {}' --bind 'focus:transform-header:file --brief {}' \
---color=bg+:#37393e,bg:#111318,spinner:#ffb4ab,hl:#bec6dc \
---color=fg:#e2e2e9,header:#bec6dc,info:#dcbce0,pointer:#ffb4ab \
---color=marker:#aac7ff,fg+:#e2e2e9,prompt:#dcbce0,hl+:#bec6dc \
---color=selected-bg:#aac7ff \
---color=border:#8e9099,label:#e2e2e9 \
+--color=bg+:#3d373d,bg:#161217,spinner:#ffb4ab,hl:#d4c0d7 \
+--color=fg:#e9e0e7,header:#d4c0d7,info:#f5b7b5,pointer:#ffb4ab \
+--color=marker:#e4b7f3,fg+:#e9e0e7,prompt:#f5b7b5,hl+:#d4c0d7 \
+--color=selected-bg:#e4b7f3 \
+--color=border:#978e97,label:#e9e0e7 \
 --multi"
 
 
 # Aloxaf fzf - tab
-zstyle ':fzf-tab:*' fzf-flags --color=bg+:#37393e,bg:#111318,spinner:#ffb4ab,hl:#bec6dc,fg:#e2e2e9,header:#bec6dc,info:#dcbce0,pointer:#ffb4ab,marker:#aac7ff,fg+:#e2e2e9,prompt:#dcbce0,hl+:#bec6dc,selected-bg:#aac7ff,border:#8e9099,label:#e2e2e9 --multi
+zstyle ':fzf-tab:*' fzf-flags --color=bg+:#3d373d,bg:#161217,spinner:#ffb4ab,hl:#d4c0d7,fg:#e9e0e7,header:#d4c0d7,info:#f5b7b5,pointer:#ffb4ab,marker:#e4b7f3,fg+:#e9e0e7,prompt:#f5b7b5,hl+:#d4c0d7,selected-bg:#e4b7f3,border:#978e97,label:#e9e0e7 --multi
