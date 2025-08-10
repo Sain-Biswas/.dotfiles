ec5fceaf79921ff98407ccc005ad8d68cc310812
Sain-Biswas <sain.biswas614@gmail.com>
2025-08-10 13:44:23 +0530

    feat: add ghostty vercel theme

--- scripts/theme/wall-apply.sh
+++ scripts/theme/wall-apply.sh
@@ -19,7 +19,7 @@ notify-send "Changing Theme" "Applying new wallpaper and updating colors, please
 swww img "$IMAGE" --transition-type="center"
 
 # Generate pywal 16 colors
-wal -i "$IMAGE"
+wal --cols16 -i "$IMAGE"
 
 # Apply matugen Theme
 matugen image "$IMAGE"
@@ -28,8 +28,8 @@ matugen image "$IMAGE"
 cat ~/.cache/wal/ghostty.conf > ~/.dotfiles/.config/ghostty/themes/pywal
 
 # Update pywal colors to warp terminal
-mkdir -p ~/.local/share/warp-terminal/themes/
-cat ~/.cache/wal/colors-warp-terminal.yml > ~/.local/share/warp-terminal/themes/pywal_16.yml
+# mkdir -p ~/.local/share/warp-terminal/themes/
+# cat ~/.cache/wal/colors-warp-terminal.yml > ~/.local/share/warp-terminal/themes/pywal_16.yml
 
 # Generate Telegram Theme
 wal-telegram --wal
