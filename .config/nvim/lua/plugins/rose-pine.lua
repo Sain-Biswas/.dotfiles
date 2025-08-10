b6a03ded33a3ca1373139a112d1d728793935baf
Sain-Biswas <sain.biswas614@gmail.com>
2025-08-10 18:57:24 +0530

    feat: add lazy to nvim

---
+++ .config/nvim/lua/plugins/rose-pine.lua
@@ -0,0 +1,27 @@
+return {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    config = function()
+        require("rose-pine").setup({
+            variant = "main",      -- auto, main, moon, or dawn
+            dark_variant = "main", -- main, moon, or dawn
+            dim_inactive_windows = false,
+            extend_background_behind_borders = true,
+
+            highlight_groups = {
+                TelescopeBorder = { fg = "highlight_high", bg = "none" },
+                TelescopeNormal = { bg = "none" },
+                TelescopePromptNormal = { bg = "base" },
+                TelescopeResultsNormal = { fg = "subtle", bg = "none" },
+                TelescopeSelection = { fg = "text", bg = "base" },
+                TelescopeSelectionCaret = { fg = "rose", bg = "rose" },
+            },
+
+            styles = {
+                bold = true,
+                italic = true,
+                transparency = true,
+            },
+        })
+    end
+}
